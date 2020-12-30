9cc262958a370f27e2b6f0095190ef4d52b53213
Svi-Kos <svitlanakos@gmail.com>
2020-12-30 17:00:25 +0200

    contact form переписує на хуки

--- my-app/src/components/ContactForm/ContactForm.jsx
+++ my-app/src/components/ContactForm/ContactForm.jsx
@@ -1,65 +1,131 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
+// import { Component } from 'react';
 import s from '../ContactForm/ContactForm.module.css';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleChange = event => {
+  const handleChange = event => {
     const { name, value } = event.currentTarget;
-    this.setState({
-      [name]: value,
-    });
+
+    switch (name) {
+      case 'name':
+        setName(value);
+
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    const name = this.state.name;
-    const number = this.state.number;
+    onSubmit(name, number);
 
-    this.props.onSubmit(name, number);
-
-    this.reset();
+    reset();
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className={s.contactForm}>
-        <label className={s.contactFormLabel}>
-          Name
-          <input
-            type="text"
-            required
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label className={s.contactFormLabel}>
-          Number
-          <input
-            type="tel"
-            pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
-            required
-            name="number"
-            value={this.state.number}
-            onChange={this.handleChange}
-          />
-          <p className={s.numberFormat}>Format: xxx-xx-xx</p>
-        </label>
-        <button type="submit" className={s.contactFormBtn}>
-          Add contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit} className={s.contactForm}>
+      <label className={s.contactFormLabel}>
+        Name
+        <input
+          type="text"
+          required
+          name="name"
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <label className={s.contactFormLabel}>
+        Number
+        <input
+          type="tel"
+          pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
+          required
+          name="number"
+          value={number}
+          onChange={handleChange}
+        />
+        <p className={s.numberFormat}>Format: xxx-xx-xx</p>
+      </label>
+      <button type="submit" className={s.contactFormBtn}>
+        Add contact
+      </button>
+    </form>
+  );
 }
+
+// class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   handleChange = event => {
+//     const { name, value } = event.currentTarget;
+//     this.setState({
+//       [name]: value,
+//     });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+
+//     // const name = this.state.name;
+//     // const number = this.state.number;
+
+//     // this.props.onSubmit(name, number);
+
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({ name: '', number: '' });
+//   };
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit} className={s.contactForm}>
+//         <label className={s.contactFormLabel}>
+//           Name
+//           <input
+//             type="text"
+//             required
+//             name="name"
+//             value={this.state.name}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         <label className={s.contactFormLabel}>
+//           Number
+//           <input
+//             type="tel"
+//             pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
+//             required
+//             name="number"
+//             value={this.state.number}
+//             onChange={this.handleChange}
+//           />
+//           <p className={s.numberFormat}>Format: xxx-xx-xx</p>
+//         </label>
+//         <button type="submit" className={s.contactFormBtn}>
+//           Add contact
+//         </button>
+//       </form>
+//     );
+//   }
+// }
 
 export default ContactForm;
